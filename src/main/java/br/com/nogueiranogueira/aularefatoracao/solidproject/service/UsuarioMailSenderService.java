00b363a04227ed36f5d47eeedad7c3cc0d370ee0
mayhmemo <mayhm.social@proton.me>
2026-02-18 22:11:07 -0300

    fix: corrige mensagem do email de boas vindas

--- src/main/java/br/com/nogueiranogueira/aularefatoracao/solidproject/service/UsuarioMailSenderService.java
+++ src/main/java/br/com/nogueiranogueira/aularefatoracao/solidproject/service/UsuarioMailSenderService.java
@@ -1,6 +1,5 @@
 package br.com.nogueiranogueira.aularefatoracao.solidproject.service;
 
-import br.com.nogueiranogueira.aularefatoracao.solidproject.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,20 +7,11 @@ import org.springframework.stereotype.Service;
 public class UsuarioMailSenderService {
 
     @Autowired
-    private UsuarioRepository usuarioRepository;
-
     private SmtpEmailService smtpEmailService;
 
-    public UsuarioMailSenderService() {
-        this.smtpEmailService = new SmtpEmailService();
-    }
-
-    public void enviarEmailBoasVindas(String email) {
-        // Lógica para enviar email de boas-vindas
+    public void enviarEmailBoasVindas(String email, String nomeUsuario) {
         String assunto = "Bem-vindo ao nosso sistema!";
-        String mensagem = "Olá! Obrigado por se cadastrar em nosso sistema.";
+        String mensagem = "Olá, " + nomeUsuario + ", seu cadastro foi realizado com sucesso.";
         smtpEmailService.sendEmail(email, assunto, mensagem);
     }
-
-
 }
