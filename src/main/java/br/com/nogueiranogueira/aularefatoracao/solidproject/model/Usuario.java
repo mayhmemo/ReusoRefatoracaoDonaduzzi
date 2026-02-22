d5fb8f48cd78a5befb65c775252d257a455450b0
mayhmemo <mayhm.social@proton.me>
2026-02-22 18:33:25 -0300

    refactor: aplica Getter e Setter

--- src/main/java/br/com/nogueiranogueira/aularefatoracao/solidproject/model/Usuario.java
+++ src/main/java/br/com/nogueiranogueira/aularefatoracao/solidproject/model/Usuario.java
@@ -2,7 +2,11 @@ package br.com.nogueiranogueira.aularefatoracao.solidproject.model;
 
 import br.com.nogueiranogueira.aularefatoracao.solidproject.interfaces.Descontavel;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
 @Entity
 @Table
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
@@ -22,40 +26,7 @@ public class Usuario implements Descontavel {
         this.tipo = tipo;
     }
 
-    public Usuario() {
-
-    }
-
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public String getNome() {
-        return nome;
-    }
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    public TipoUsuario getTipo() {
-        return tipo;
-    }
-    public void setTipo(TipoUsuario tipo) {
-        this.tipo = tipo;
-    }
-    public int getPontos() {
-        return pontos;
-    }
-    public void setPontos(int pontos) {
-        this.pontos = pontos;
-    }
+    public Usuario() {}
 
     @Override
     public int getDesconto() {
