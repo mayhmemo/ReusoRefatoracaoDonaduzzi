b6d70aa2369e659bfc0275c131fa80a75205e7e6
mayhmemo <mayhm.social@proton.me>
2026-02-24 10:27:19 -0300

    refactor: remove funções e repositories desnecessários

--- src/main/java/br/com/nogueiranogueira/aularefatoracao/solidproject/interfaces/IUsuarioRepository.java
+++ src/main/java/br/com/nogueiranogueira/aularefatoracao/solidproject/interfaces/IUsuarioRepository.java
@@ -1,11 +1,8 @@
 package br.com.nogueiranogueira.aularefatoracao.solidproject.interfaces;
 
-import br.com.nogueiranogueira.aularefatoracao.solidproject.model.TipoUsuario;
 import br.com.nogueiranogueira.aularefatoracao.solidproject.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IUsuarioRepository extends JpaRepository<Usuario, Long> {
-    long countByTipo(TipoUsuario tipoUsuario);
-
     long countByEmail(String email);
 }
