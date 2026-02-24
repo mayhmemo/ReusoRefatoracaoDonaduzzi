0c3c8afb981d7ac8aa93cc1b1aefea1ecffc5e09
mayhmemo <mayhm.social@proton.me>
2026-02-24 10:27:30 -0300

    refactor: remove id sem sentido

--- src/main/java/br/com/nogueiranogueira/aularefatoracao/solidproject/dto/UsuarioDTO.java
+++ src/main/java/br/com/nogueiranogueira/aularefatoracao/solidproject/dto/UsuarioDTO.java
@@ -3,7 +3,6 @@ package br.com.nogueiranogueira.aularefatoracao.solidproject.dto;
 import br.com.nogueiranogueira.aularefatoracao.solidproject.model.TipoUsuario;
 
 public record UsuarioDTO(
-        Long id,
         String nome,
         String email,
         TipoUsuario tipo,
